113f3647fa963713de9825dc897b5a3e4bb43dd4
bilalimrani1 <78067566+bilalimrani1@users.noreply.github.com>
2021-06-21 00:03:16 +0500

    changes

--- index.js
+++ index.js
@@ -1,71 +1,71 @@
 const express = require('express');
 const nodemailer = require('nodemailer'),
-  bodyParser = require('body-parser'),
-  path = require('path');
+	bodyParser = require('body-parser'),
+	path = require('path');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get('/api', (req, res) => {
-  console.log('fsdfsddsfasg');
-  res.send({ message: 'Hello from server!' });
+	console.log('fsdfsddsfasg');
+	res.send({ message: 'Hello from server!' });
 });
 
 let transporter = nodemailer.createTransport({
-  host: 'smtp.gmail.com',
-  port: 465,
-  secure: true,
-  ignoreTLS: false,
-  auth: {
-    user: 'bimrani816@gmail.com',
-    pass: 'rana7441634',
-  },
+	host: 'smtp.gmail.com',
+	port: 465,
+	secure: true,
+	ignoreTLS: false,
+	auth: {
+		user: 'bimrani816@gmail.com',
+		pass: 'rana7441634',
+	},
 });
 
 // async..await is not allowed in global scope, must use a wrapper
 async function main(email, subject, description, phone, name) {
-  console.log('HHHHHHHHHHYYYYYYYyyyyy');
+	console.log('HHHHHHHHHHYYYYYYYyyyyy');
 
-  let info = await transporter.sendMail({
-    from: 'bimrani816@gmail.com',
-    to: 'bimrani816@gmail.com, mshahrukh@live.com',
-    subject: subject,
-    html: `<div>Name: ${name || 'N/A'}</div><div>Phone: ${
-      phone || 'N/A'
-    }</div><div>Email ${email || 'N/A'}</div><div>Description: ${
-      description || 'N/A'
-    }</div>`,
-  });
+	let info = await transporter.sendMail({
+		from: 'bimrani816@gmail.com',
+		to: 'bimrani816@gmail.com, mshahrukh@live.com',
+		subject: subject,
+		html: `<div>Name: ${name || 'N/A'}</div><div>Phone: ${
+			phone || 'N/A'
+		}</div><div>Email ${email || 'N/A'}</div><div>Description: ${
+			description || 'N/A'
+		}</div>`,
+	});
 
-  console.log('Message sent: %s', info.messageId);
-  console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+	console.log('Message sent: %s', info.messageId);
+	console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
 }
 
 app.post('/hireus', (req, res) => {
-  const { email, description, phone, name } = req.body;
-  console.log(req.body, req.params);
-  main(email, 'Hire Us', description, phone, name);
+	const { email, description, phone, name } = req.body;
+	console.log(req.body, req.params);
+	main(email, 'Hire Us', description, phone, name);
 
-  res.send({ status: 200 });
+	res.send({ status: 200 });
 });
 
 app.post('/contactus', (req, res) => {
-  const { email, description, phone, name } = req.body;
-  console.log(req.body, req.params);
-  main(email, 'Contact Us', description, phone, name);
+	const { email, description, phone, name } = req.body;
+	console.log(req.body, req.params);
+	main(email, 'Contact Us', description, phone, name);
 
-  res.send({ status: 200 });
+	res.send({ status: 200 });
 });
 
 app.post('/subscribe', async (req, res) => {
-  const { email } = req.body;
-  console.log(req.body);
-  let info = await transporter.sendMail({
-    from: 'bimrani816@gmail.com',
-    to: 'bimrani816@gmail.com, mshahrukh@live.com',
-    subject: 'UXTClan Subscription',
-    html: `<div style="background-color:#ececec;padding:0;margin:0 auto;font-weight:200;width:100%!important">
+	const { email } = req.body;
+	console.log(req.body);
+	let info = await transporter.sendMail({
+		from: 'bimrani816@gmail.com',
+		to: 'bimrani816@gmail.com, mshahrukh@live.com',
+		subject: 'UXTClan Subscription',
+		html: `<div style="background-color:#ececec;padding:0;margin:0 auto;font-weight:200;width:100%!important">
          <table align="center" border="0" cellspacing="0" cellpadding="0" style="table-layout:fixed;font-weight:200;font-family:Helvetica,Arial,sans-serif" width="100%">
             <tbody>
                <tr>
@@ -163,20 +163,20 @@ app.post('/subscribe', async (req, res) => {
             </tbody>
          </table>
       </div>`,
-  });
+	});
 
-  console.log('Message sent: %s', info.messageId);
-  console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+	console.log('Message sent: %s', info.messageId);
+	console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
 
-  res.send({ status: 200 });
+	res.send({ status: 200 });
 });
 
-app.use(express.static(path.join(__dirname + '/client/build')));
+app.use(express.static(path.join('./client/build')));
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/build', 'index.html'));
+	res.sendFile(path.join('./client/build', 'index.html'));
 });
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
-  console.log(`Server listening on ${port}`);
+	console.log(`Server listening on ${port}`);
 });
